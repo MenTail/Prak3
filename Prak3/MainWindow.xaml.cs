b07ff78bf6b5d3556b8400a660d770be6b30f4dc
MenTail <dzaman2016@gmail.com>
2020-10-20 13:19:40 +0300

    rw

--- Prak3/MainWindow.xaml.cs
+++ Prak3/MainWindow.xaml.cs
@@ -15,22 +15,33 @@ using System.Windows.Shapes;
 
 namespace Prak3
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            gor.IsChecked = true;
         }
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool BS1 = (bool) rog.IsChecked;
+            bool BS2 = (bool) H1.IsChecked; 
+            int BS3 = 0;
+            switch (Log.Text)
+            {
+                case "Дерево": BS3 = 2; break;
+                case "Метал": BS3 = 0; break;
+                case "Металопластик": BS3 = 1; break;
+            }
+            double x = Convert.ToDouble(Bss1.Text)* Convert.ToDouble(Bss2.Text);
+            double y = (0.05 + 0.1* BS3) + (0.05 * Convert.ToInt32(BS1));
+            Bart.Content = "Вартість: " + (x*y+(35 * Convert.ToInt32(BS2))) + " грн";
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
 
         }
